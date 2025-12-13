82d63b4453387de2616082c5be6e36a73de75e53
Artiom Cozlovici <kozakovi4@outlook.com>
2025-12-13 13:15:20 +0200

    feat: Register custom User and Group admin classes for enhanced admin functionality

--- src/shared/admin.py
+++ src/shared/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import (
+    GroupAdmin as BaseGroupAdmin,
+)
+from django.contrib.auth.admin import (
+    UserAdmin as BaseUserAdmin,
+)
+from django.contrib.auth.models import Group
+from unfold.admin import ModelAdmin
 
-# Register your models here.
+User = get_user_model()
+
+
+admin.site.unregister(User)
+admin.site.unregister(Group)
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin, ModelAdmin):
+    pass
+
+
+@admin.register(Group)
+class GroupAdmin(BaseGroupAdmin, ModelAdmin):
+    pass
