475efddd782bca75227fa0d8c7d9b28e9fa93aa6
Artiom Cozlovici <kozakovi4@outlook.com>
2025-12-14 21:13:22 +0200

    feat: Add signals for Certificate model to handle pre-save rendering

--- src/course/apps.py
+++ src/course/apps.py
@@ -1,5 +1,10 @@
+from importlib import import_module
+
 from django.apps import AppConfig
 
 
 class CourseConfig(AppConfig):
     name = "course"
+
+    def ready(self):
+        import_module("course.signals")
---
+++ src/course/signals.py
@@ -0,0 +1,17 @@
+from typing import Type
+
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+
+from course.models import Certificate
+from course.services import CertificateRenderService
+
+
+@receiver(pre_save, sender=Certificate)
+def certificate_pre_save(sender: Type[Certificate], instance: Certificate, **kwargs):
+    if instance.tracker.has_changed("file") and instance.tracker.previous("file") is None:
+        return
+
+    if set(instance.tracker.changed().keys()) & {"first_name", "last_name", "intake"}:
+        service = CertificateRenderService(certificate=instance)
+        service.render()
