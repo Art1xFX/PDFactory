d6393e7964419d9f2c20aa19916461b947670e72
Artiom Cozlovici <kozakovi4@outlook.com>
2025-12-14 13:38:07 +0200

    feat: Register Course, Intake, and Certificate models in admin

--- src/course/admin.py
+++ src/course/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
 
-# Register your models here.
+from course.models import Certificate, Course, Intake
+
+
+@admin.register(Course)
+class CourseAdmin(ModelAdmin):
+    pass
+
+
+@admin.register(Intake)
+class IntakeAdmin(ModelAdmin):
+    pass
+
+
+@admin.register(Certificate)
+class CertificateAdmin(ModelAdmin):
+    pass
