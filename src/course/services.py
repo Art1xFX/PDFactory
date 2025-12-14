3d684d37e84696ca6a651177a3da8a4c11f75529
Artiom Cozlovici <kozakovi4@outlook.com>
2025-12-14 21:12:00 +0200

    feat: Implement CertificateRenderService for rendering certificates to HTML

---
+++ src/course/services.py
@@ -0,0 +1,31 @@
+from django.core.files.base import ContentFile
+from django.template import engines
+from django.utils.text import slugify
+
+from course.models import Certificate
+
+
+class CertificateRenderService:
+    def __init__(self, certificate: Certificate):
+        self.certificate = certificate
+
+    def render(self):
+        certificate = self.certificate
+        intake = certificate.intake
+        course = intake.course
+
+        html = (
+            engines["django"]
+            .from_string(intake.template.content)
+            .render(
+                context={
+                    "certificate": certificate,
+                }
+            )
+        )
+
+        filename = f"{course.id}/{intake.id}/{slugify(certificate.last_name)}_{slugify(certificate.first_name)}.html"
+
+        certificate.file.save(filename, ContentFile(html.encode("utf-8")))
+
+        return ContentFile(html.encode("utf-8"))
