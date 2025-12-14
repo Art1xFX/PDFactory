03fada4424290bd1ca49bd7ae6734878664dba16
Artiom Cozlovici <kozakovi4@outlook.com>
2025-12-14 22:05:49 +0200

    feat: Enhance Certificate admin with detailed list display and custom queryset methods

--- src/course/admin.py
+++ src/course/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.db.models import Value
+from django.db.models.functions import Concat
 from simple_history.admin import SimpleHistoryAdmin
 from unfold.admin import ModelAdmin
 
@@ -17,4 +19,34 @@ class IntakeAdmin(ModelAdmin, SimpleHistoryAdmin):
 
 @admin.register(Certificate)
 class CertificateAdmin(ModelAdmin, SimpleHistoryAdmin):
-    pass
+    list_display = (
+        "id",
+        "full_name",
+        "intake__course",
+        "intake__start_date",
+        "intake__end_date",
+        "created_at",
+        "updated_at",
+    )
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related("intake__course")
+
+    @admin.display(
+        description="Full name",
+        ordering=Concat("first_name", Value(" "), "last_name"),
+    )
+    def full_name(self, obj):
+        return f"{obj.first_name} {obj.last_name}"
+
+    @admin.display(description="Course", ordering="intake__course__title")
+    def intake__course(self, obj):
+        return obj.intake.course
+
+    @admin.display(description="Start date", ordering="intake__start_date")
+    def intake__start_date(self, obj):
+        return obj.intake.start_date
+
+    @admin.display(description="End date", ordering="intake__end_date")
+    def intake__end_date(self, obj):
+        return obj.intake.end_date
