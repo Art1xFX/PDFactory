8bd5b166c309052dc310232ece3436ae8876927c
Artiom Cozlovici <kozakovi4@outlook.com>
2026-02-22 19:37:11 +0000

    feat: Enhance admin interface by adding FontInline and FontFamilyAdmin with historical tracking

--- src/file/admin.py
+++ src/file/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from unfold.admin import ModelAdmin
+from simple_history.admin import SimpleHistoryAdmin
+from unfold.admin import ModelAdmin, TabularInline
 
-from file.models import File
+from file.forms import FontModelForm
+from file.models import File, Font, FontFamily
 
 
 @admin.register(File)
@@ -9,3 +11,21 @@ class FileAdmin(ModelAdmin):
     list_display = ("id", "name", "file", "created_at", "updated_at")
     readonly_fields = ("created_at", "updated_at")
     search_fields = ("name",)
+
+
+class FontInline(TabularInline):
+    model = Font
+    form = FontModelForm
+
+    ordering_field = "position"  # type: ignore[assignment]
+    hide_ordering_field = True
+
+    extra = 1
+
+
+@admin.register(FontFamily)
+class FontFamilyAdmin(ModelAdmin, SimpleHistoryAdmin):
+    list_display = ("name", "created_at", "updated_at")
+    readonly_fields = ("created_at", "updated_at")
+    search_fields = ("name",)
+    inlines = [FontInline]
