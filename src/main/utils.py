c19d31d8b148b41e5f81895b7c9dba6ced5d0b9c
Artiom Cozlovici <kozakovi4@outlook.com>
2026-04-05 09:47:23 +0000

    feat: Add utility functions for translation and URL reversal in Dynaconf settings

---
+++ src/main/utils.py
@@ -0,0 +1,37 @@
+from dynaconf.utils.parse_conf import Lazy
+
+
+def _format_translation(value, **context):  # pylint: disable=unused-argument
+    from django.utils.translation import gettext_lazy as _  # pylint: disable=import-outside-toplevel
+
+    return _(value)
+
+
+def _format_link(value, **context):  # pylint: disable=unused-argument
+    from django.urls import reverse_lazy as reverse  # pylint: disable=import-outside-toplevel
+
+    return reverse(**value)
+
+
+def gettext_lazy(value):
+    """
+    Wrapper around Django's gettext_lazy to be used in Dynaconf settings.
+
+    See: https://github.com/dynaconf/dynaconf/issues/648
+    """
+    return Lazy(value, formatter=_format_translation)
+
+
+def reverse_lazy(viewname, query=None):
+    """
+    Wrapper around Django's reverse_lazy to be used in Dynaconf settings.
+
+    See: https://github.com/dynaconf/dynaconf/issues/648
+    """
+    return Lazy(
+        {
+            "viewname": viewname,
+            "query": query,
+        },
+        formatter=_format_link,
+    )
