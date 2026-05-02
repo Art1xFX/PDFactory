1b542b695d3f0378da04d2799c2cf3cd14731017
Artiom Cozlovici <kozakovi4@outlook.com>
2026-05-02 16:13:04 +0000

    feat: Enhance UserAdmin with dynamic fieldsets and readonly fields based on permissions

--- src/user/admin.py
+++ src/user/admin.py
@@ -3,6 +3,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import Group
+from django.utils.translation import gettext_lazy as _
 from guardian.admin import GuardedModelAdmin
 from guardian.shortcuts import get_objects_for_user
 from simple_history.admin import SimpleHistoryAdmin
@@ -91,6 +92,32 @@ class UserAdmin(BaseUserAdmin, GuardedModelAdmin, ModelAdmin, SimpleHistoryAdmin
                 accept_global_perms=False,
             ).exists()
         )
+
+    def get_fieldsets(self, request, obj=None):
+        fieldsets = super().get_fieldsets(request, obj)
+        perms = ["user.view_user", "user.change_user"]
+
+        if not any(request.user.has_perm(perm, obj) for perm in perms):
+            fieldsets = [
+                fieldset
+                for fieldset in fieldsets
+                if fieldset[0]
+                in (
+                    None,
+                    _("Personal info"),
+                    _("Important dates"),
+                )
+            ]
+
+        return fieldsets
+
+    def get_readonly_fields(self, request, obj=...):
+        readonly_fields = super().get_readonly_fields(request, obj)
+
+        if not request.user.has_perm("user.change_user", obj):
+            readonly_fields += ("username", "last_login", "date_joined")
+
+        return readonly_fields
 
 
 @admin.register(Group)
